1941a29d221d23cc1e964c22cf40b47a47b5b116
eiffelaqila <16520324@std.stei.itb.ac.id>
2021-11-24 17:49:49 +0700

    menambahkan kasus reject

---
+++ src/test/input6Reject.py
@@ -0,0 +1,33 @@
+# test case: def, False, True, None, return, not, with, as, class
+
+define indexOf(list, val):
+    i = 0
+    p = FIRST(l)
+    found = False
+
+    while(p != None and not found):
+        if(val == INFO(p)):
+            found = True
+        else:
+            i = i + 1
+            p = NEXT(P)
+
+    if(found):
+        return i
+    else:
+        return 99999
+
+#-----------------------------------------------------------
+# a simple file writer object
+  
+class MessageWriter(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+  
+    def __exit__(self):
+        self.file.close()
+  
+# using with statement with MessageWriter
+  
+with MessageWriter('my_file.txt') as xfile:
+    xfile.write('hello world')
